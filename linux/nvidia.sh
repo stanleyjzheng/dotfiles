39979a5f1d5ebac75f451259a9b48356343e200e
Stanley Zheng <58539993+stanleyjzheng@users.noreply.github.com>
2021-01-25 16:55:35 -0700

    Fix nvidia

--- linux/nvidia.sh
+++ linux/nvidia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo script must be run with sudo
 
-apt install system76-nvidia-drivers
+apt install system76-driver-nvidia
 apt install system76-cuda-latest
 apt install system76-cudnn-10.2
